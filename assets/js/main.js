d2bec7c2ac978c978e6bd258ed5edc3d52ee5332
Diego Mauricio Pardo Calderón <128651145+DiegoPardoCalderon@users.noreply.github.com>
2023-05-05 21:33:54 -0400

    seguimos repasando proxy en clases

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,71 @@ var simbolos = Object.getOwnPropertySymbols(persona)
 // for (const key of atributos) {
 //     console.log(key[0], " - ", key[1]);
 // }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Segunda parte viernes 05-05-2023
+
+var equipo1 = {
+    marca:"IBM",
+    modelo: "R400",
+    ram: 8,
+    disco: 240,
+    tipo: "HDD"
+}
+
+var equipo2 = {
+    marca:"Asus",
+    modelo: "A15",
+    ram: 32,
+    disco: 500,
+    tipo: "SDD"
+}
+
+var proxyEquipo1 = new Proxy(equipo1, {
+    get: function(objeto, propiedad){
+        if(propiedad == "tipo"){
+            return objeto[propiedad].toUpperCase();
+        }else {
+            return objeto[propiedad]
+        }
+    },
+    set: function(objeto, propiedad, nuevo_valor){
+        switch (propiedad) {
+            case "ram": 
+                if(nuevo_valor != 8 && nuevo_valor != 16 && nuevo_valor != 32){
+                    throw "El valores de la RAM debe ser 8, 16 o 32"
+                }
+                
+            break;
+
+            case "disco":
+                if(nuevo_valor != 120 && nuevo_valor != 240 && nuevo_valor != 500){
+                    throw "el valor de DISCO debe ser 120, 240 o 500"
+                }
+
+            break;
+
+            case "tipo":
+                if(nuevo_valor.toUpperCase() != "SSD" && nuevo_valor.toUpperCase() != "HDD"){
+                    throw "el valor de tipo debe ser SDD o HDD"
+                }
+
+            break;
+        
+            default:
+                break;
+        }
+        objeto[propiedad] = nuevo_valor
+    }
+})
+
+try {
+    proxyEquipo1.ram = 16
+    proxyEquipo1.disco= 500
+    proxyEquipo1.tipo= "HDD"
+    console.log(proxyEquipo1);
+    console.log(proxyEquipo1.tipo);
+    
+} catch (error) {
+    console.log(error);
+}
+
